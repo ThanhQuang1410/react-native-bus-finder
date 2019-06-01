64d2e3b0f44ccae610b2b54ff7e73f094b8d871a
ThanhQuang1410 <ttquang1410@gmail.com>
2019-06-01 11:14:56 +0700

    add firebase

---
+++ firebaseConnect.js
@@ -0,0 +1,12 @@
+import firebase from 'react-native-firebase'
+var firebaseConfig = {
+    apiKey: "AIzaSyAdMJzMrA_WOCEK9IYgLj13k7YdM89gYkU",
+    authDomain: "busfinder-1552206585305.firebaseapp.com",
+    databaseURL: "https://busfinder-1552206585305.firebaseio.com",
+    projectId: "busfinder-1552206585305",
+    storageBucket: "busfinder-1552206585305.appspot.com",
+    messagingSenderId: "398689985179",
+    appId: "1:398689985179:web:dea8a6e7f5a5c1b3"
+};
+// Initialize Firebase
+export default firebaseConnect = firebase.initializeApp(firebaseConfig);
